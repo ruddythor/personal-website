68f8f5d7e2a6a82b9496c649d6d5658ba9d46572
Josh Brown <josh.s.brown@hotmail.com>
2012-08-10 00:02:21 -0400

    made the string format characters into self.XXXXX

--- newclasses.py
+++ newclasses.py
@@ -11,7 +11,7 @@ class Circle():
 		self.r=r
 		self.stroke_width=stroke_width
 		circle = open(self.filename, "w")
-		circle.writelines("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" stroke=\"black\" stroke-width=\"%d\" fill=\"none\"/>" % (cx, cy, r, stroke_width))
+		circle.writelines("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" stroke=\"black\" stroke-width=\"%d\" fill=\"none\"/>" % (self.cx, self.cy, self.r, self.stroke_width))
 		circle.close()
 
 circle=Circle("newxml.xml")
