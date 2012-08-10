e9194bec70a7d131abe4e87da4e430f33465a524
Josh Brown <josh.s.brown@hotmail.com>
2012-08-10 00:01:06 -0400

    moved the filename up into the __init__

--- newclasses.py
+++ newclasses.py
@@ -2,20 +2,20 @@
 
 
 class Circle():
-	def __init__(self):
+	def __init__(self, filename):
 		Name="Circle"
-	def circling(self, filename, cx, cy, r, stroke_width):
 		self.filename=filename
+	def circling(self, cx, cy, r, stroke_width):
 		self.cx=cx
 		self.cy=cy
 		self.r=r
 		self.stroke_width=stroke_width
-		circle = open(filename, "w")
+		circle = open(self.filename, "w")
 		circle.writelines("<circle cx=\"%d\" cy=\"%d\" r=\"%d\" stroke=\"black\" stroke-width=\"%d\" fill=\"none\"/>" % (cx, cy, r, stroke_width))
 		circle.close()
 
-circle=Circle()
-circle.circling("newxml.xml", 100, 100, 100, 2)
+circle=Circle("newxml.xml")
+circle.circling(100, 100, 100, 2)
 
 
 
