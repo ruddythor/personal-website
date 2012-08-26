ef1ae22de7dd3323af4ca1bdba5d9d70c22d4494
Josh Brown <josh.s.brown@hotmail.com>
2012-08-26 01:10:40 -0400

    Did more with classes, created some objects and cleaned up methods

--- rpg.py
+++ rpg.py
@@ -12,12 +12,16 @@ class Die:
 		return 'Dieroll'
 #	@staticmethod
 	def roll(self, dice, sides):
-#		for x in self.myrolls:
-#			self.myrolls.remove(x)
+		for x in self.myrolls:
+			self.myrolls.remove(x)
+		self.total=0
 		for x in range(1, dice+1):
 			self.myrolls.append(random.randrange(1, sides))
 		for x in self.myrolls:
+			self.total+=x
 			print x,
+#		print "This is the total for the die roll"
+#		print self.total
 		return
 
 
@@ -25,26 +29,27 @@ class Player:
 	def __init__(self):
 		Name = ""
 		die=Die()
-		self.hproll=die.roll(4, 6)
-		self.hpmax=0
-		for x in self.hproll:
-			self.hpmax += x
-		print self.hpmax
-		self.base_attack=5
-		self.base_defense=3
+		print "this is the die roll"
+		self.hproll=die.roll(15, 6)
+		self.hpmax=die.total
+#		for x in self.hproll:
+#			self.hpmax += x
+		self.base_attack=3
+		self.base_defense=2
 		self.equipment=[]
 		self.equipped_items=[]
-
 
 	def attack(self):
 		attack_value=self.base_attack
 		for item in self.equipped_items:
-			attack_value+=item.attack
+			attack_value+=item.attack_value
+		return attack_value
 
 	def defend(self):
 		defense_value=self.base_defense
 		for item in self.equipped_items:
-			defense_value+=item.defense
+			defense_value+=item.defense_value
+		return defense_value
 
 	def add(self, item):
 		self.equipment.append(item)
@@ -53,16 +58,34 @@ class Player:
 		self.equipment.remove(item)
 
 	def equip(self, item):
-		if self.equipment.index(item):
-			self.equipped_items.append(item)
+#		if len(self.equipped_items)>3:
+#			self.equipped_items.remove()#ITEMNUMBERONE
+		self.equipped_items.append(item)
+#		if self.equipment.index(item):
+#			self.equipped_items.append(item)
 
+	def display_inventory(self):
+		for item in self.equipment:
+			print item
+		return
+
+	def display_equipped(self):
+		print "These are your equipped items"
+		for item in self.equipped_items:
+			print item
+		return
 
 playerone=Player()
-print playerone.hp
+print "This is the player's max HP"
+print playerone.hpmax
+print "This is the player's base attack value"
 print playerone.base_attack
+print "This is the player's base defense value"
 print playerone.base_defense
-playerone.attack()#THIS WILL RETURN ATTACK VALUE 
-playerone.defend()#RETURNS DEFENSE VALUE
+print "This is the player's attack value"
+print playerone.attack()#THIS WILL RETURN ATTACK VALUE 
+print "This is the player's defense value"
+print playerone.defend()#RETURNS DEFENSE VALUE
 
 #FUNCTION FOR ACTUAL COMBAT
 
@@ -78,6 +101,7 @@ class Equipment:
 		return self.name
 	def __repr__(self):
 		return self.name
+
 	def to_string(self):
 		return self.name.join(self.attack_value)
 
@@ -86,104 +110,46 @@ class Equipment:
 	def defend(self):
 		return str(self.defend_value)
 
-longsword=Equipment(10, 0, "Long sword", "A bad-ass slayer of women and small men")
+
+#======================ESTABLISHING WEAPONS AND ARMOR================================
+
+longsword=Equipment(10, 0, "long sword", "A bad-ass slayer of women and small men")
+print "This is the longsword's attack value"
 print longsword.attack()
 shortsword=Equipment(7, 0, "short sword", "A not-so-bad-ass slayer of women and small men")
+print "this is the short sword's attack value"
+print shortsword.attack()
+
+lightarmor=Equipment(0, 5, "light armor", "A simple piece of armor that barely protects you. It is very simple looking.")
+
+heavyarmor=Equipment(0,15, "heavy armor", "A complex piece of armor that protects you strongly.")
 
 
 
 
 
-gold=100
-strength=10
-healthmax=15
-hp=healthmax
-movement=5
-charlevel=1
-defense=0
-roll=Die()
-attack=0
-
-inventory=[]
-weapons=[shortsword, longsword]
-armor=[]
-goal= 0
-
-
-def actionfunc():
-	global goal
-	action=raw_input("\n\nWhat would you like to do? ['fight', 'move', 'drop item', 'pick up', 'equip [weapon, armor]', 'unequip [weapon, armor]', 'info', 'rest', 'quit']\n>>")
-	if action == "equip weapon":
-		equipweaponfunc()
-		goal += 1
-	elif action == "unequip weapon":
-		unequipweaponfunc()
-		goal = goal + 1
-
-	elif action == "equip armor":
-		equiparmorfunc()
-		goal+=1
-	elif action == "unequip armor":
-		unequiparmorfunc()
-		goal = goal + 1
-	elif action == "fight":
-		fight()
-		goal = goal + 1
-	elif action == "quit":
-		quit()
-		goal = goal + 1
-	elif action == "drop item":
-		dropitem()
-		goal = goal + 1
-	elif action == "move":
-		move()
-		goal = goal + 1
-	elif action == "pickup item":
-		pickup()
-		goal = goal + 1
-	elif action == "rest":
-		goal = goal + 1
-		rest()
-	elif action == "info":
-		info()
-
-	else:
-		print "Invalid instruction. Please try again."
-		actionfunc()
-
-
-def rest():
-	global hp
-	global healthmax
-	hp=healthmax
-
-def quit():
-	global goal
-	goal=5
-
-
-
-def info():
-	global healthmax, hp, inventory, weapons, armor, gold, strength, movement, charlevel, equippedarmor, equippedweapon, defense, attack
-
-	print "your character is level", charlevel
-	print "Your current health is ", hp, "out of", healthmax
-	print "You have", gold, "gold pieces."
-	print "Your inventory consists of:"
-	for x in inventory:
-		print "\t", x
-	for x in weapons:
-		print "\t", x
-	for x in armor:
-		print "\t", x
-	print "Your strength is", strength
-	print "Your movement value is", movement
-	print "your defense value is", defense
-	print "Your attack value is", attack
-	print "you have equipped,", equippedweapon, equippedarmor
 
 
 
 
-#while goal <=5:
-#	actionfunc()
+playerone.add(longsword)
+playerone.add(shortsword)
+playerone.add(lightarmor)
+playerone.add(heavyarmor)
+playerone.equip(shortsword)
+playerone.equip(lightarmor)
+playerone.display_inventory()
+playerone.display_equipped()
+
+print "NOW, this is the player's attack value, because we equipped a short sword"
+print playerone.attack()
+
+playerone.equip(longsword)
+playerone.display_equipped()
+print "NOW, this is the player's attack value, because we equipped a Long sword"
+print playerone.attack()
+print playerone.defend()
+
+
+
+
