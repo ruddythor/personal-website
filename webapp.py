912ba36dbce848ac8eb9e1bad95a88373f440c97
Josh Brown <josh.s.brown@hotmail.com>
2012-08-28 17:14:52 -0400

    removed some files

---
+++ webapp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def application(environment, start_response):
+
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, hello_world)
+    srv.serve_forever()
+	
+
+
