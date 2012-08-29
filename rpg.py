37de74d9ce3a596cfa6b3a15702a517178e3937c
Josh Brown <josh.s.brown@hotmail.com>
2012-08-28 23:54:59 -0400

    Can now purchase things

--- rpg.py
+++ rpg.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import random
 import string
+import time
+
 
 class Die:
 	def __init__(self):
@@ -37,6 +39,10 @@ class Player:
 		self.base_defense=2
 		self.equipment=[]
 		self.equipped_items=[]
+		self.gold=50
+#		self.right_hand=[]
+#		self.left_hand=[]
+#		self.head=[]
 
 	def attack(self):
 		attack_value=self.base_attack
@@ -88,6 +94,17 @@ class Player:
 		for item in self.equipped_items:
 			print "\t", item
 		return
+
+	def buy(self, item):
+		self.equipment.append(item)
+		self.gold=self.gold-item.buy_value
+
+	def sell(self, item):
+		self.equipment.remove(item)
+		self.gold+=item.sell_value
+
+
+
 #===========================INSTANTIATES PLAYERONE OF THE PLAYER CLASS AND TESTS SOME OF THE METHODS AND VARIABLES=========================
 playerone=Player("Charles")
 print playerone.Name
@@ -106,12 +123,14 @@ print playerone.defend()
 
 #========================CLASS DEFINING EQUIPMENT CREATION===================================s
 class Equipment:
-	def __init__(self, attack_value, defense_value, name, description, is_armor):
+	def __init__(self, attack_value, defense_value, name, description, is_armor, buy_value, sell_value):
 		self.is_armor=is_armor
 		self.name=name
 		self.description=description
 		self.attack_value=attack_value
 		self.defense_value=defense_value
+		self.buy_value=buy_value
+		self.sell_value=sell_value
 
 	def __str__(self):
 		return self.name
@@ -129,18 +148,19 @@ class Equipment:
 
 #======================ESTABLISHING WEAPONS AND ARMOR================================
 
-longsword=Equipment(10, 0, "long sword", "A bad-ass slayer of women and small men", False)
+longsword=Equipment(10, 0, "long sword", "A bad-ass slayer of women and small men", False, 50, 15)
 print "This is the longsword's attack value"
 print longsword.attack()
-shortsword=Equipment(7, 0, "short sword", "A not-so-bad-ass slayer of women and small men", False)
+shortsword=Equipment(7, 0, "short sword", "A not-so-bad-ass slayer of women and small men", False, 25, 7)
 print "this is the short sword's attack value"
 print shortsword.attack()
 
-lightarmor=Equipment(0, 5, "light armor", "A simple piece of armor that barely protects you. It is very simple looking.", True)
+dagger=Equipment(4, 0, "dagger", "A small dagger, capable of killing someone, but only if you get really cloes to them.", False, 15, 4)
+lightarmor=Equipment(0, 5, "light armor", "A simple piece of armor that barely protects you. It is very simple looking.", True, 65, 20)
+heavyarmor=Equipment(0,15, "heavy armor", "A complex piece of armor that protects you strongly.", True, 100, 25)
 
-heavyarmor=Equipment(0,15, "heavy armor", "A complex piece of armor that protects you strongly.", True)
-
-
+global available_items
+available_items={1:dagger, 2:shortsword, 3:longsword, 4:lightarmor, 5:heavyarmor}
 
 
 
@@ -148,43 +168,89 @@ heavyarmor=Equipment(0,15, "heavy armor", "A complex piece of armor that protect
 
 
 #===========================================TESTING MORE FUNCTIONS OF THE PLAYER CLASS. THEY ALL SEEM TO WORK==========================
-playerone.add(longsword)
-playerone.add(shortsword)
-playerone.add(lightarmor)
-playerone.add(heavyarmor)
-playerone.equip(shortsword)
-playerone.equip(lightarmor)
+#playerone.add(longsword)
+#playerone.add(shortsword)
+#playerone.add(lightarmor)
+#playerone.add(heavyarmor)
+#playerone.equip(shortsword)
+#playerone.equip(lightarmor)
+#playerone.display_inventory()
+#playerone.display_equipped()
+
+#print "NOW, this is the player's attack value, because we equipped a short sword"
+#print playerone.attack()
+#playerone.unequip(shortsword)
+#playerone.equip(longsword)
+#playerone.display_equipped()
+#print "NOW, this is the player's attack value, because we equipped a Long sword"
+#print playerone.attack()
+#print playerone.defend()
+
+
+#=========================================NOW FOR THE FUNCTIONS OF THE GAME=============================================
+def main_menu():
+	print """
+
+
+
+You have the following options:
+
+\t1: Enter the store.
+\t2: Enter the arena to fight.
+\t3: Display player information.
+\t4: Change weapon and armor loadout.
+\t5: Quit
+
+                                Enter a number and press enter."""
+	player_choice=input("\n>>")
+	if player_choice==1:
+		store()
+	elif player_choice==2:
+		arena()
+	elif player_choice==3:
+		info()
+	elif player_choice==4:
+		change_equipment()
+	else:
+		main_menu()
+
+def store():
+	global available_items
+	print """
+
+
+
+
+
+Enter 1 to buy something. Enter 2 to sell something."""
+	buy_or_sell=input("\n>>")
+	if buy_or_sell==1:
+		buy()
+	elif buy_or_sell==2:
+		sell()
+
+def buy():
+	global available_items
+	print """
+
+
+
+
+The following items are available for purchase:"""
+	for key in available_items:
+		print key, ":", available_items[key], available_items[key].buy_value
+
+	print """You have %i gold.
+Enter the number of the item you'd like to purchase.""" %playerone.gold
+	purchase_item=input(">>")
+	playerone.add(available_items[purchase_item])
+
+
+main_menu()
+
+#============================================TESTING THE BUY FUNCTION==============================
+
+playerone.display_equipped()
+print "These are your inventory items"
 playerone.display_inventory()
-playerone.display_equipped()
 
-print "NOW, this is the player's attack value, because we equipped a short sword"
-print playerone.attack()
-playerone.unequip(shortsword)
-playerone.equip(longsword)
-playerone.display_equipped()
-print "NOW, this is the player's attack value, because we equipped a Long sword"
-print playerone.attack()
-print playerone.defend()
-
-
-playerone.equip(heavyarmor)
-playerone.display_equipped()
-print
-print
-playerone.equip(shortsword)
-playerone.add(longsword)
-playerone.equip(longsword)
-print playerone.equipped_items
-print len(playerone.equipped_items)
-print playerone.attack()
-playerone.equip(heavyarmor)
-print playerone.equipped_items
-playerone.equip(longsword)
-playerone.equip(longsword)
-playerone.equip(shortsword)
-print playerone.equipped_items
-playerone.equip(heavyarmor)
-print playerone.equipped_items
-print playerone.attack()
-print playerone.defend()
-print "that was the defense value. number above is attack"
