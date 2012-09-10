21ece316e399b248f81c1e3e86511047df62459a
Josh Brown <josh.s.brown@hotmail.com>
2012-09-09 23:43:37 -0400

    Added an enemy class based off the player class, messed with attack method

--- rpg.py
+++ rpg.py
@@ -21,7 +21,7 @@ class Die:
 			self.myrolls.append(random.randrange(1, sides))
 		for x in self.myrolls:
 			self.total+=x
-			print x,
+#			print x,
 #		print "This is the total for the die roll"
 #		print self.total
 		return
@@ -32,7 +32,7 @@ class Player:
 	def __init__(self, Name):
 		self.Name=Name
 		die=Die()
-		print "this is the die roll"
+#		print "this is the die roll"
 		self.hproll=die.roll(15, 6)
 		self.hpmax=die.total
 		self.base_attack=3
@@ -48,7 +48,11 @@ class Player:
 		attack_value=self.base_attack
 		for item in self.equipped_items:
 			attack_value+=item.attack_value
-		return attack_value
+#		return attack_value
+		hitdice=Die()
+		hitdice.roll(attack_value, 6)
+		attack=hitdice.total
+		return attack
 
 	def defend(self):
 		defense_value=self.base_defense
@@ -111,17 +115,17 @@ class Player:
 
 #===========================INSTANTIATES PLAYERONE OF THE PLAYER CLASS AND TESTS SOME OF THE METHODS AND VARIABLES=========================
 playerone=Player("Charles")
-print playerone.Name
-print "This is the player's max HP"
-print playerone.hpmax
-print "This is the player's base attack value"
-print playerone.base_attack
-print "This is the player's base defense value"
-print playerone.base_defense
-print "This is the player's attack value"
-print playerone.attack()
-print "This is the player's defense value"
-print playerone.defend()
+#print playerone.Name
+#print "This is the player's max HP"
+#print playerone.hpmax
+#print "This is the player's base attack value"
+#print playerone.base_attack
+#print "This is the player's base defense value"
+#print playerone.base_defense
+#print "This is the player's attack value"
+#print playerone.attack()
+#print "This is the player's defense value"
+#print playerone.defend()
 
 #FUNCTION FOR ACTUAL COMBAT
 
@@ -153,11 +157,11 @@ class Equipment:
 #======================ESTABLISHING WEAPONS AND ARMOR================================
 
 longsword=Equipment(10, 0, "long sword", "A bad-ass slayer of women and small men", False, 50, 15)
-print "This is the longsword's attack value"
-print longsword.attack()
+#print "This is the longsword's attack value"
+#print longsword.attack()
 shortsword=Equipment(7, 0, "short sword", "A not-so-bad-ass slayer of women and small men", False, 25, 7)
-print "this is the short sword's attack value"
-print shortsword.attack()
+#print "this is the short sword's attack value"
+#print shortsword.attack()
 
 dagger=Equipment(4, 0, "dagger", "A small dagger, capable of killing someone, but only if you get really cloes to them.", False, 15, 4)
 lightarmor=Equipment(0, 5, "light armor", "A simple piece of armor that barely protects you. It is very simple looking.", True, 65, 20)
@@ -167,17 +171,101 @@ global available_items
 available_items={1:dagger, 2:shortsword, 3:longsword, 4:lightarmor, 5:heavyarmor}
 
 
+#===========================================ESTABLISHING SOME ENEMIES==========================
+
+
+
+
+class Enemy:
+	def __init__(self, Name, hp_dice, base_attack, base_defense):
+		self.Name=Name
+		die=Die()
+#p		print "this is the die roll"
+		self.hproll=die.roll(hp_dice, 6)
+		self.myrolls=die.myrolls
+		self.hpmax=die.total
+		self.base_attack=base_attack
+		self.base_defense=base_defense
+		self.equipment=[]
+		self.equipped_items=[]
+#		self.right_hand=[]
+#		self.left_hand=[]
+#		self.head=[]
+
+	def attack(self):
+		attack_value=self.base_attack
+		for item in self.equipped_items:
+			attack_value+=item.attack_value
+#		return attack_value
+		hitdice=Die()
+		hitdice.roll(attack_value, 6)
+		attack=hitdice.total
+		return attack
+
+
+
+
+	def defend(self):
+		defense_value=self.base_defense
+		for item in self.equipped_items:
+			defense_value+=item.defense_value
+		return defense_value
+
+	def add(self, item):
+		self.equipment.append(item)
+
+	def drop(self, item):
+		self.equipment.remove(item)
+
+	def equip(self, item):
+		weapons=0
+		armors=0
+		for x in self.equipped_items:
+			if x.is_armor==True:
+				armors+=1
+			else:
+				weapons+=1
+
+		if len(self.equipped_items)>2:
+			self.equipped_items.pop(0)
+
+		if self.equipment.index(item):
+			if armors<=0 and item.is_armor==True:
+				self.equipped_items.append(item)
+			elif weapons<=1 and item.is_armor==False:
+				self.equipped_items.append(item)
+		else:
+			print "You have too many of that type of item equipped. Try again."
+		# if self.equipment.index(item):
+		# self.equipped_items.append(item)
+
+	def unequip(self, item):
+		self.equipped_items.remove(item)
+
+	def display_inventory(self):
+		for item in self.equipment:
+			print "\t", item
+		return
+
+	def display_equipped(self):
+		print "These are your equipped items"
+		for item in self.equipped_items:
+			print "\t", item
+		return
+
+
+
 
 
 
 
 #===========================================TESTING MORE FUNCTIONS OF THE PLAYER CLASS. THEY ALL SEEM TO WORK==========================
-#playerone.add(longsword)
-#playerone.add(shortsword)
-#playerone.add(lightarmor)
-#playerone.add(heavyarmor)
-#playerone.equip(shortsword)
-#playerone.equip(lightarmor)
+playerone.add(longsword)
+playerone.add(shortsword)
+playerone.add(lightarmor)
+playerone.add(heavyarmor)
+playerone.equip(shortsword)
+playerone.equip(lightarmor)
 #playerone.display_inventory()
 #playerone.display_equipped()
 
@@ -191,6 +279,34 @@ available_items={1:dagger, 2:shortsword, 3:longsword, 4:lightarmor, 5:heavyarmor
 #print playerone.defend()
 
 
+
+
+
+rat=Enemy("A Rat", 10, 1, 1)
+#hitdice=Die()
+#hitdice.roll(rat.attack(), 6)
+#attack=hitdice.total
+#print attack
+print 
+print
+print rat.myrolls
+print
+print
+print rat.attack()
+print
+print
+print rat.hpmax, rat.base_attack, rat.base_defense
+print 
+print "This is the player one's attack roll"
+print playerone.attack()
+
+
+
+
+
+
+
+
 #=========================================NOW FOR THE FUNCTIONS OF THE GAME=============================================
 
 
@@ -198,7 +314,7 @@ available_items={1:dagger, 2:shortsword, 3:longsword, 4:lightarmor, 5:heavyarmor
 
 
 
-#class Menu:
+#class MenuClass:
 #	def __init__(self):
 #		Name="Menu Class"
 
@@ -293,6 +409,51 @@ The following items are equippable by you:"""
 		print "\t", x
 	print "\nWhat would you like to equip?"
 	equip_this=input(">>")
+
+
+
+#def arena():
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
