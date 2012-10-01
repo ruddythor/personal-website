39b9c4e9941ea9745e5ac23d0012340ca0b08592
Josh <josh.s.brown@hotmail.com>
2012-09-30 20:32:28 -0400

    removed extra files

---
+++ dice.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 27, 2012
+
+@author: josh
+'''
+import random
+class Die:
+    def __init__(self):
+        myclass='this is the die roll class'
+        self.myrolls=[]
+    def __str__(self):
+        return 'Dieroll'
+    def __repr__(self):
+        return 'Dieroll'
+
+    def roll(self, dice, sides):
+        for x in self.myrolls:
+            self.myrolls.remove(x)
+        self.total=0
+        for x in range(1, dice+1):
+            self.myrolls.append(random.randrange(1, sides))
+        for x in self.myrolls:
+            self.total+=x
+        return
