9e8a4eaebd369d9944f23fcb046ddec7af86920b
Josh <josh.s.brown@hotmail.com>
2012-09-30 20:43:58 -0400

    removed unused variable

--- dice.py
+++ dice.py
@@ -6,7 +6,6 @@ Created on Sep 27, 2012
 import random
 class Die:
     def __init__(self):
-        myclass='this is the die roll class'
         self.myrolls=[]
     def __str__(self):
         return 'Dieroll'
