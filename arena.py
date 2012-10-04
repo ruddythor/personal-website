533ed46727d63b6bcfdd40db14775e9dc5365ef5
Josh <josh.s.brown@hotmail.com>
2012-10-03 23:24:42 -0400

    made everything into a function to cut down on length

--- arena.py
+++ arena.py
@@ -15,7 +15,7 @@ def fight():
     from enemylist import enemylist
 # PICKS AN ENEMY FROM ENEMYLIST FILE
     opponent=random.choice(enemylist)
-#    global enemy_hp, playerone_hp
+    global enemy_hp, playerone_hp
     opponent.hpmax=opponent.hpmax
     enemy_hp=opponent.hpmax
     enemy_defend=opponent.defend()
@@ -38,29 +38,13 @@ def fight():
         
         if player_initiative > opponent_initiative:
             print "Player goes first."
-            if your_attack>enemy_defend:
-                print "player lands attack"
-                your_attack=your_attack-enemy_defend
-                enemy_hp=enemy_hp-your_attack
-                print enemy_hp
-#            playerone_attack_check(your_attack, enemy_defend, enemy_hp, playerone_hp)
+            playerone_attack_check(your_attack, enemy_defend, enemy_hp, playerone_hp)
             if enemy_hp<=0:
-                if player.playerone.xp>=level_threshhold:
-                    print "Checking for level up"
-                    player.playerone.level+=1
-                    player.playerone.hpmax+=player.playerone.hpmax*.15#NEED TO ROUND UP!!!!
-                    level_threshhold=level_threshhold*.2+level_threshhold*2
-                    print "**** You leveled up! ****\n your new level is: ", player.playerone.level, "your new level threshhold", level_threshhold
-#                check_for_levelup(level_threshhold)
+                check_for_levelup(level_threshhold)
                 print "you killed the enemy, returning to main menu"
                 break
 
-#            enemy_attack_check(enemy_attack, your_defend, playerone_hp, enemy_hp)
-            if enemy_attack>your_defend:
-                print "opponent lands attack"
-                enemy_attack=enemy_attack-your_defend
-                playerone_hp=playerone_hp-enemy_attack
-                print playerone_hp            
+            enemy_attack_check(enemy_attack, your_defend, playerone_hp, enemy_hp)
             if playerone_hp<=0:
                 print "You died. Returning to main menu"
                 menus.main()
@@ -72,37 +56,23 @@ def fight():
         
         elif opponent_initiative>player_initiative:
             print "Opponent goes first."
-#            enemy_attack_check(enemy_attack, your_defend, playerone_hp, enemy_hp)
-            if enemy_attack>your_defend:
-                print "opponent lands attack"
-                enemy_attack=enemy_attack-your_defend
-                playerone_hp=playerone_hp-enemy_attack
-                print playerone_hp      
+            enemy_attack_check(enemy_attack, your_defend, playerone_hp, enemy_hp)
             if playerone_hp<=0:
                 print"You died. Returning to main menu"
                 menus.main()
-#            playerone_attack_check(your_attack, enemy_defend, enemy_hp, playerone_hp)
-            if your_attack>enemy_defend:
-                print "player lands attack"
-                your_attack=your_attack-enemy_defend
-                enemy_hp=enemy_hp-your_attack
-                print enemy_hp
-#            playerone_attack_check(your_attack, enemy_defend, enemy_hp, playerone_hp)
+            playerone_attack_check(your_attack, enemy_defend, enemy_hp, playerone_hp)
             if enemy_hp<=0:
-                if player.playerone.xp>=level_threshhold:
-                    print "Checking for level up"
-                    player.playerone.level+=1
-                    player.playerone.hpmax+=player.playerone.hpmax*.15#NEED TO ROUND UP!!!!
-                    level_threshhold=level_threshhold*.2+level_threshhold*2
-                    print "**** You leveled up! ****\n your new level is: ", player.playerone.level, "your new level threshhold", level_threshhold
+                check_for_levelup(level_threshhold)
                 print "you killed the enemy, returning to main menu"
                 menus.main()
+                break
             print "Your hp: ", playerone_hp, "Enemy's hp: ", enemy_hp
         print "END OF TURN\n-----------------------------"
     menus.main()
 
 
 def enemy_attack_check(enemy_attack, your_defend, player_hp, enemy_hp):
+    global playerone_hp
     if enemy_attack>your_defend:
         print "opponent lands attack"
         enemy_attack=enemy_attack-your_defend
@@ -110,6 +80,7 @@ def enemy_attack_check(enemy_attack, your_defend, player_hp, enemy_hp):
         print playerone_hp
 
 def playerone_attack_check(your_attack, enemy_defend, enem_hp, playerone_hp):
+    global enemy_hp
     if your_attack>enemy_defend:
         print "player lands attack"
         your_attack=your_attack-enemy_defend
@@ -117,6 +88,7 @@ def playerone_attack_check(your_attack, enemy_defend, enem_hp, playerone_hp):
         print enemy_hp
 
 def check_for_levelup(threshhold):
+    global level_threshhold
     print "about to check for level up"
     if player.playerone.xp>=level_threshhold:
         print "Checking for level up"
@@ -124,20 +96,4 @@ def check_for_levelup(threshhold):
         player.playerone.hpmax+=player.playerone.hpmax*.15#NEED TO ROUND UP!!!!
         level_threshhold=threshhold*.2+threshhold*2
         print "**** You leveled up! ****\n your new level is: ", player.playerone.level, "your new level threshhold", level_threshhold
-
-
-
-
-#player_xp, player_hpmax, level_threshhold------stuff for check for levelup
-
-
-
-
-
-
-
-
-
-
-
 
