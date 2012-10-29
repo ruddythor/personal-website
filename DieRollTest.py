af448bdc8f1c939f0595f026ada26913494adbbc
Matthew J. Brown <matthew_brown34@hotmail.com>
2012-10-28 21:39:51 -0400

    updated DieRollTest to display all info generated for the enemy

--- DieRollTest.py
+++ DieRollTest.py
@@ -16,8 +16,12 @@ while gameExit != True:
 	
 	generator = enemyGen.Generator()
 	
-	name, hp = generator.getEnemy()
-	print "", name, " was generated with ", hp, " HP." 
+	name, hp, baseAttack, baseDefense, xpVal, renownVal, goldDie = generator.getEnemy()
+	print "", name, " was generated with ", hp, " HP."
+	print "\nThe enemy's other stats are:\n"
+	print "\tBase Attack: ", baseAttack, "\n\tBase Defense: ", baseDefense
+	print "\n\tXP Value: ", xpVal, "\n\tRenown Value: ", renownVal
+	print "\n\tGold Drop Value: ", goldDie		
 	choice = 0    	
 	while choice < 1 or choice > 2:	
 		print "Continue? (1 = yes/2 = no)"
