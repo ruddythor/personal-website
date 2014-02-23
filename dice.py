35039cf98ea38f4829655b547daafbb97d71ddb9
Josh B <josh.s.brown@hotmail.com>
2014-02-23 01:27:12 -0500

    improving dice module

--- dice.py
+++ dice.py
@@ -6,19 +6,13 @@ Created on Sep 27, 2012
 '''
 import random
 class Die:
-    def __init__(self):
-        self.myrolls=[]
     def __str__(self):
         return 'Dieroll'
     def __repr__(self):
         return 'Dieroll'
 
     def roll(self, dice, sides):
-        for x in self.myrolls:
-            self.myrolls.remove(x)
-        self.total=0
+        total=0
         for x in range(1, dice+1):
-            self.myrolls.append(random.randrange(1, sides))
-        for x in self.myrolls:
-            self.total+=x
-        return
+            total += random.randrange(1, sides)
+        return total
