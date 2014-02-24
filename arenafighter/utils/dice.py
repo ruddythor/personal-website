df246d72bf4ae30b4709dbdd164d38ae18f27ef8
Josh B <josh.s.brown@hotmail.com>
2014-02-24 17:52:12 -0500

    dependencies

---
+++ arenafighter/utils/dice.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+Created on Sep 27, 2012
+
+@author: josh
+'''
+import random
+class Die():
+    def __str__(self):
+        return 'Dieroll'
+    def __repr__(self):
+        return 'Dieroll'
+
+    def roll(self, dice, sides):
+        total=0
+        for x in range(1, dice+1):
+            total += random.randrange(1, sides)
+        return total
