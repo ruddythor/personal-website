8b0c54cba8c99bc38b16739b31f8953c91bffb97
Josh B <josh.s.brown@hotmail.com>
2014-02-24 23:20:01 -0500

    improving dice algorithm

--- arenafighter/utils/dice.py
+++ arenafighter/utils/dice.py
@@ -5,14 +5,14 @@ Created on Sep 27, 2012
 @author: josh
 '''
 import random
-class Die():
-    def __str__(self):
-        return 'Dieroll'
-    def __repr__(self):
-        return 'Dieroll'
 
-    def roll(self, dice, sides):
-        total=0
-        for x in range(1, dice+1):
-            total += random.randrange(1, sides)
-        return int(total)
+def __str__(self):
+    return 'Dieroll'
+def __repr__(self):
+    return 'Dieroll'
+
+def roll(dice, sides):
+    total=0
+    for x in range(1, dice+1):
+        total += random.randrange(1, sides+1)
+    return int(total)
