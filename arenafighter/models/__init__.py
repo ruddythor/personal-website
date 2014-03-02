05561b080454476baf6a21b7d362e88c370e6b74
Josh B <josh.s.brown@hotmail.com>
2014-03-02 12:04:46 -0500

    renaming files

--- arenafighter/models/__init__.py
+++ arenafighter/models/__init__.py
@@ -1,4 +1,4 @@
 from arenafighter.models.character import *
-from arenafighter.models.equipment import *
+from arenafighter.models.inventory import *
 from arenafighter.models.enemy import *
 from arenafighter.models.profile_model import *
