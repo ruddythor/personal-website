607fccdd1106c6a5dcfa8f1be1b57d834b031e8d
Josh B <josh.s.brown@hotmail.com>
2014-03-14 01:55:41 -0400

    getting store purchasing working better

--- arenafighter/urls.py
+++ arenafighter/urls.py
@@ -10,21 +10,30 @@ urlpatterns = patterns('',
     url(r'^info/(\d+)/$', 'arenafighter.views.home.info', name='player_info'),
     url(r'^arena$', 'arenafighter.views.home.go_to_arena', name='arena'),
 
-    url(r'^purchase/(?P<item_type>.+)/(?P<id>\d+)$', 'arenafighter.views.store.buy', name='buy'),
+    url(r'^purchase/(?P<item_type>.+)/(?P<id>\d+)$', 'arenafighter.views.store.buy', name='buy_item'),
+    url(r'^sell/(?P<item_type>.+)/(?P<id>\d+)$', 'arenafighter.views.store.sell', name='sell_item'),
+
+
+    url(r'^sell_shop_item/(.+)$', 'arenafighter.views.store.item_detail', {'sell': True}, name='sell_item_detail'),
+    url(r'^sell_shop_weapon/(.+)$', 'arenafighter.views.store.weapon_detail', {'sell': True}, name='sell_weapon_detail'),
+    url(r'^sell_shop_armor/(.+)$', 'arenafighter.views.store.armor_detail', {'sell': True}, name='sell_armor_detail'),
 
     url(r'^shop_item/(.+)$', 'arenafighter.views.store.item_detail', {'store': True}, name='shop_item_detail'),
     url(r'^shop_weapon/(.+)$', 'arenafighter.views.store.weapon_detail', {'store': True}, name='shop_weapon_detail'),
     url(r'^shop_armor/(.+)$', 'arenafighter.views.store.armor_detail', {'store': True}, name='shop_armor_detail'),
-    url(r'^item/(.+)$', 'arenafighter.views.store.item_detail', {'store': False}, name='item_detail'),
-    url(r'^weapon/(.+)$', 'arenafighter.views.store.weapon_detail', {'store': False}, name='weapon_detail'),
-    url(r'^armor/(.+)$', 'arenafighter.views.store.armor_detail', {'store': False}, name='armor_detail'),
 
-    url(r'^equip/(?P<item_type>.+)/(?P<item_id>\d+)$', 'arenafighter.views.home.equip', name='equip'),
+    url(r'^item/(.+)$', 'arenafighter.views.store.item_detail', name='item_detail'),
+    url(r'^weapon/(.+)$', 'arenafighter.views.store.weapon_detail', name='weapon_detail'),
+    url(r'^armor/(.+)$', 'arenafighter.views.store.armor_detail', name='armor_detail'),
+
+
+    url(r'^sell$', 'arenafighter.views.store.character_inventory', name='sell_detail'),
 
     url(r'^fight', 'arenafighter.views.arena.fight', name='fight'),
     url(r'^fight/(?P<enemy_id>\d+)$', 'arenafighter.views.arena.fight', name='continued_fight'),
 
 
+    url(r'^equip/(?P<item_type>.+)/(?P<item_id>\d+)$', 'arenafighter.views.home.equip', name='equip'),
     url(r'^play_as/(\d+)', 'arenafighter.views.home.play_as_character', name='play_as_character'),
     url(r'^delete/(\d+)', 'arenafighter.views.home.delete', name='delete'),
     url(r'^signup/', 'arenafighter.views.home.signup', name='signup'),
--- arenafighter/views/store.py
+++ arenafighter/views/store.py
@@ -1,6 +1,7 @@
 import collections
 from django.shortcuts import render, redirect
 from arenafighter.models.inventory import Inventory, InventoryItem, Weapon, Armor
+from arenafighter.models.character import Character
 
 # TODO: DE-uglify this view function
 def shop(request, store_level):
@@ -15,46 +16,70 @@ def shop(request, store_level):
         generate_weapons(store_level)
     items = InventoryItem.objects.exclude(inventory__isnull=False)
     weapons = Weapon.objects.exclude(inventory__isnull=False)
-    armors = Armor.objects.exclude(inventory__isnull=False)
+    armor = Armor.objects.exclude(inventory__isnull=False)
     items = dict(collections.Counter([item.name for item in items]))
     weapons = dict(collections.Counter([item.name for item in weapons]))
-    armors = dict(collections.Counter([item.name for item in armors]))
+    armor = dict(collections.Counter([item.name for item in armor]))
     context = {'items': items,
                'weapons': weapons,
-               'armors': armors,
+               'armor': armor,
                }
     return render(request, 'store.html', context)
 
+
+def character_inventory(request):
+    character = Character.objects.get(id=request.user.profile.current_character_id)
+    inventory = Inventory.objects.prefetch_related('items', 'armor', 'weapons').filter(character=character)[0]
+    items = dict(collections.Counter([item.name for item in inventory.items.all()]))
+    weapons = dict(collections.Counter([item.name for item in inventory.weapons.all()]))
+    armor = dict(collections.Counter([item.name for item in inventory.armor.all()]))
+    context = {'character': character,
+               'items': items,
+               'weapons': weapons,
+               'armor': armor,
+               }
+    return render(request, 'sell_items.html', context)
+
+
 # TODO: combine these *_detail views to be better
-def item_detail(request, name, store):
+def item_detail(request, name, store=False, sell=False):
     if store:
         object = InventoryItem.objects.filter(name=name).filter(inventory_id=None)[0]
+    elif sell:
+        object = InventoryItem.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     else:
         object = InventoryItem.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     context = {'item': object,
                'store': store,
+               'sell': sell,
                }
     return render(request, 'item.html', context)
 
 
-def armor_detail(request, name, store):
+def armor_detail(request, name, store=False, sell=False):
     if store:
         object = Armor.objects.filter(name=name).filter(inventory_id=None)[0]
+    elif sell:
+        object = Armor.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     else:
         object = Armor.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     context = {'item': object,
                'store': store,
+               'sell': sell,
                }
     return render(request, 'item.html', context)
 
 
-def weapon_detail(request, name, store):
+def weapon_detail(request, name, store=False, sell=False):
     if store:
         object = Weapon.objects.filter(name=name).filter(inventory_id=None)[0]
+    elif sell:
+        object = Weapon.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     else:
         object = Weapon.objects.filter(name=name).filter(inventory_id=request.user.profile.current_character.inventory.id)[0]
     context = {'item': object,
                'store': store,
+               'sell': sell,
                }
     return render(request, 'item.html', context)
 
@@ -67,6 +92,18 @@ def buy(request, item_type, id):
         object = InventoryItem.objects.get(id=id)
     request.user.profile.current_character.purchase(object)
     return redirect('store')
+
+
+def sell(request, item_type, id):
+    if item_type == 'Armor':
+        object = Armor.objects.get(id=id)
+    elif item_type == 'Weapon':
+        object = Weapon.objects.get(id=id)
+    elif item_type == 'InventoryItem':
+        object = InventoryItem.objects.get(id=id)
+    request.user.profile.current_character.sell(object)
+    return redirect('store')
+
 
 
 def generate_items(store_level):
